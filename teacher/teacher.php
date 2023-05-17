c7a8475aa5cd227d289c22fffa88c2980b909b01
Jovan Kiš <xkis@stuba.sk>
2023-05-18 01:07:10 +0200

    fix: refactor fixes

--- teacher/teacher.php
+++ teacher/teacher.php
@@ -8,7 +8,7 @@
     <title>Math website</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../styles.css">
+    <link rel="stylesheet" href="../style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
